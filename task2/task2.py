d742cb6aff2dbf2ff38309641d7cdaec3099a8a2
Amir <Amir93@yandex.ru>
2024-09-08 16:07:37 +0300

    Update task2.py

--- task2/task2.py
+++ task2/task2.py
@@ -1,1 +1,20 @@
-pass
+files = ['circle.txt', 'dot.txt']
+handles = [open(file) for file in files]
+
+
+def fun(cir_pos, dot_pos):
+    circ = [x.split() for x in cir_pos.readlines()]
+    dot = [x.split() for x in dot_pos.readlines()]
+    for dot_data in dot:
+        result = ((int(dot_data[0]) - int(circ[0][0])) ** 2 / int(circ[1][0]) ** 2
+                  + (int(dot_data[1]) - int(circ[0][1])) ** 2 / int(circ[1][0]) ** 2)
+
+        if result < 1:
+            print(1)
+        elif result == 1:
+            print(0)
+        else:
+            print(2)
+
+
+fun(handles[0], handles[1])
