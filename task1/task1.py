45f3a5293dc1b38891ae0a842ce4e80921e93579
Amir <Amir93@yandex.ru>
2024-09-14 21:23:41 +0300

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,21 +1,19 @@
-def fun(n=int(input()), m=int(input())):
-    arr_1 = list(range(1, n + 1))
-    arr_2 = []
-    arr_3 = arr_1
-    final_num = []
-    a, b = 0, m
-
-    while arr_3[-1] != arr_1[0]:
-        if m > n:
-            print('Второй аргумент должен быть меньше или равен первому.')
-            break
-        arr_2.extend(arr_1)
-        arr_3 = arr_2[a:b]
-        new_arr = arr_2[a:b]
-        final_num.append(new_arr[0])
-        a, b = b - 1, b + m - 1
-
-    return print(''.join([str(x) for x in final_num]))
+import math
 
 
-fun()
+def fun(n, m):
+    arr = list(range(1, n + 1))
+    arr.extend(arr * int(math.ceil(m / n)))
+    a, b, c = 0, m, ''
+    while True:
+        c += str(arr[a:b][0])
+        if arr[0] == arr[a:b][-1]:
+            return print('Полученый путь: ' + c)
+        a = (b - 1) % n
+        b = a + m
+
+
+while True:
+    fun(int(input('n = ')), int(input('m = ')))
+    if input("Хотите продолжить?(y/n) = ") != 'y':
+        break
